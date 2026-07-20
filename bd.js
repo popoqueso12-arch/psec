e381e250c98705b3da82971b27794329b8ae3324
popoqueso12-arch <popoqueso12@gmail.com>
2026-07-19 21:29:09 -0500

    Update bd.js

--- bd.js
+++ bd.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'mysql-86c4508-ja13-1fe5.a.aivencloud.com',
-  port: process.env.DB_PORT || 26767,
+  host: process.env.DB_HOST || 'mysql--ja13-1fe5.a.aivencloud.com',
+  port: process.env.DB_PORT || ,
   user: process.env.DB_USER || 'avnadmin',
-  password: process.env.DB_PASSWORD || 'default_password',
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'defaultdb',
   waitForConnections: true,
   connectionLimit: 10,

